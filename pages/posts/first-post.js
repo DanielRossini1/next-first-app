dbe572dd4228a3c717476a7bd33ee86e00c0b851
Daniel Rossini <danielrossini7@gmail.com>
2021-01-22 06:00:12 -0300

    implementado ssg e ssr

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,13 +2,27 @@ import Link from 'next/link'
 import Head from 'next/head'
 import Layout from '../../components/layout'
 
-export default function FirstPost() {
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export async function getServerSideProps(context) {
+  await sleep(2000);
+  return {
+    props: {
+      teste: 'teste'
+    }
+  }
+}
+
+export default function FirstPost({ teste }) {
   return (
     <Layout>
       <Head>
         <title>First post</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {teste}
       <h1>First Post</h1>
       <h2>
         <Link href="/">
